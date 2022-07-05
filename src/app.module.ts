15b50e329376b5f11b92d87b8b54eb6a4237d69b
Vinicius Rosa <viniciusrosa3525@gmail.com>
2022-07-05 16:05:10 -0300

    Add service and database operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdutosController } from './produtos.controller';
-import { ProdutosService } from './produtos.service';
+import { Livro } from './livro.model';
+import { LivrosController } from './livros.controller';
+import { LivrosService } from './livros.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProdutosController],
-  providers: [AppService, ProdutosService],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro]),
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
 })
 export class AppModule {}
---
+++ src/livros.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { Livro } from './livro.model';
+import { LivrosService } from './livros.service';
+
+@Controller('livros')
+export class LivrosController {
+  constructor(private livrosService: LivrosService) {}
+
+  @Get()
+  async obterTodos(): Promise<Livro[]> {
+    return this.livrosService.obterTodos();
+  }
+
+  @Get(':id')
+  async obterUm(@Param() params): Promise<Livro> {
+    return this.livrosService.obterUm(params.id);
+  }
+
+  @Post()
+  async criar(@Body() livro: Livro) {
+    this.livrosService.criar(livro);
+  }
+
+  @Put()
+  async alterar(@Body() livro: Livro): Promise<[number]> {
+    return this.livrosService.alterar(livro);
+  }
+
+  @Delete(':id')
+  async apagar(@Param() params) {
+    this.livrosService.apagar(params.id);
+  }
+}
